037bd9ee15045050db73ca06777f6420d377a68c
DoTheCau <work.docau@gmail.com>
2025-07-06 22:08:48 +0700

    Add CodePipeline step

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,12 +1,23 @@
 import * as cdk from 'aws-cdk-lib'
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new cdk.aws_codecommit.Repository(this, 'WorkshopRepo', {
-            repositoryName: 'WorkshopRepo'
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'WorkshopPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.gitHub('https://github.com/DoCau/cdk-workshop.git', 'master', {
+                    authentication: cdk.SecretValue.secretsManager('githubtoken')
+                }),
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            }),
         });
     }
 }
